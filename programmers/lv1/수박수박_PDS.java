850a2147b14ca9a62f9c24cd56eb4aba4d4854db
pds0309 <ehd0309@gmail.com>
2021-12-27 11:49:07 +0900

    add [프로그래머스] 수박수박 / 박동석

---
+++ programmers/lv1/수박수박_PDS.java
@@ -0,0 +1,11 @@
+public class 수박수박 {
+
+  public String solution(int n) {
+    String answer = "";
+    String[] text = {"수", "박"};
+    for (int i = 0; i < n; i++) {
+      answer += text[i % 2];
+    }
+    return answer;
+  }
+}
