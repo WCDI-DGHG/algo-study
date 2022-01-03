905243724f81450ee980d6a534fabeeed9b61992
KYEONGRYONG OH <96464756+okr4306@users.noreply.github.com>
2022-01-04 01:11:24 +0900

    Add files via upload

---
+++ workshop01/MyTest02.java
@@ -0,0 +1,11 @@
+package workshop01;
+
+public class MyTest02 {
+
+    public static void main(String[] args) {
+        String JuminNum = "921219-1234567";
+        System.out.println("나의 주민번호 :" + JuminNum);
+
+    }
+
+}
