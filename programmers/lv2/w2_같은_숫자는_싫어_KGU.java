f1233ad7543d6146249a201bf99de862aa0b4246
GounKim <kgukgu2001@gmail.com>
2022-01-03 22:19:15 +0900

    add [프로그래머스] 같은 숫자는 싫어 / 김고운

---
+++ programmers/lv2/w2_같은_숫자는_싫어_KGU.java
@@ -0,0 +1,31 @@
+/* 
+같은 숫자는 싫어
+https://programmers.co.kr/learn/courses/30/lessons/12906
+Code_By_Goun
+*/
+
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        int[] answer = {};
+        ArrayList<Integer> arrList = new ArrayList<Integer>();
+        arrList.add(arr[0]);
+        
+        for (int i = 1, m = 0; i < arr.length; i++) {
+            if (arrList.get(m) == arr[i]) {
+                continue;
+            }
+            arrList.add(arr[i]);
+            m++;
+        }
+        
+        int sizeOfList = arrList.size();
+        answer = new int[sizeOfList];
+        for(int i = 0; i < sizeOfList; i++) {
+            answer[i] = arrList.get(i);
+        }
+
+        return answer;
+    }
+}
