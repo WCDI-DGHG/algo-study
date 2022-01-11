4246cf5e7872a51efe42d80ee9adfa165058c2da
GounKim <kgukgu2001@gmail.com>
2022-01-11 22:45:24 +0900

    add [백준] 고양이 / 김고운

---
+++ baekjoon/입출력과_사칙연산/w3_10171_고양이_KGU.java
@@ -0,0 +1,20 @@
+/*
+BAEKJOON
+10171번_고양이
+Code_By_Goun
+*/
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write( "\\    /\\\n"
+				+ " )  ( \')\n"
+				+ "(  /  )\n"
+				+ " \\(__)|");
+		bw.flush();
+		bw.close();
+	}
+}
+
