b9b431f6a7a7cb630673cf5a0a0e2ba7b903f71a
GounKim <kgukgu2001@gmail.com>
2022-01-14 02:21:18 +0900

    add [프로그래머스] 주식가격 / 김고운

---
+++ programmers/lv2/w3_주식가격_KGU.java
@@ -0,0 +1,44 @@
+/* 
+주식가격
+알고리즘_스택과 큐
+Code_By_Goun
+*/
+
+import java.util.Stack;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = {};
+        Stack<Integer> stack = new Stack<Integer>();
+        int len = prices.length;
+        answer = new int[len];
+        
+        for (int i = 0; i < len; i++) {
+        	if (stack.isEmpty()) {
+        		stack.push(i);
+        	} else {
+        		int index = stack.pop();
+        		while (prices[index] > prices[i]) {
+        			answer[index] = i - index;
+        			if (!stack.isEmpty()) {
+        				index = stack.pop();
+        			} else {
+        				index = i;
+        			}
+        		}
+        		
+        		if(index != i) {
+        			stack.push(index);
+        		}
+        		stack.push(i);
+        	}
+        }
+        
+        while (!stack.isEmpty()) {
+        	int index = stack.pop();
+        	answer[index] = (len - 1) - index;
+        }
+                
+        return answer;
+    }
+}
