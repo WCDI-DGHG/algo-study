f1cd594705ce5bdb7144579858e68795f9c1ccfd
GounKim <kgukgu2001@gmail.com>
2022-02-08 00:29:39 +0900

    Revert "Merge branch 'kgu' of https://github.com/WCDI-DGHG/algo-study into kgu"
    This reverts commit fc0de11db9b4d00e3e971af989244c6f845e041b, reversing
    changes made to 416acc766519703661f26ae689892f48fa243a95.

--- programmers/lv2/w2_자릿수_더하기_PDS.java
+++
@@ -1,7 +0,0 @@
-import java.util.*;
-public class Solution {
-    public int solution(int n) {
-        String result = n + "";
-        return result.chars().sum() - 48 * result.length();
-    }
-}
--- programmers/lv2/w2_정수_제곱근_판별_PDS.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public long solution(long n) {
-        double result = Math.sqrt(n);
-        if(result % 1 == 0){
-            return (long)Math.pow(result+1,2);
-        }
-        return -1;
-    }
-}
