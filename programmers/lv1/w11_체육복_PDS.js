1eaea4d797d983a980e9cdcc3508e74ada30234b
pds0309 <ehd0309@gmail.com>
2022-03-19 00:17:58 +0900

    fix [프로그래머스] 체육복 / 박동석

--- programmers/lv1/w11_체육복_PDS.js
+++ programmers/lv1/w11_체육복_PDS.js
@@ -1,33 +1,28 @@
+const set = (lost, reserve) => {
+    reserve.forEach((val,j) => {
+       lost.find((v,i)=>{
+         if(val === v){
+             reserve.splice(j,1);
+             lost.splice(i,1);
+             set(lost,reserve);
+         }  
+       });
+    });
+}
+
 function solution(n, lost, reserve) {
     lost.sort();
     reserve.sort();
     
-    for(let i = 0; i < reserve.length; i ++){
-        for(let j = 0; j < lost.length; j ++){
-            if(lost[j] == reserve[i]) {
-                lost.splice(j,1);
-                reserve.splice(i,1);
-                i=0;j=0;
-            }
-        }
-    }
+    set(lost,reserve);
+
     let count = n - lost.length;
     lost.forEach((val,j) => {
         reserve.find((v,i) => {
-            if(v === val){
+            if(v - 1 <= val && v + 1 >= val ){
                 reserve.splice(i,1);
                 count++;
-                return;
             }            
-           if(v - 1 === val) {
-                reserve.splice(i,1);
-                count++;
-               return;
-           } 
-           if(v + 1 === val) {
-                reserve.splice(i,1);
-                count++;
-            }
         });
     });
     
